3a49d54d115ad750e0886025d7332ff419b15c36
ranaaditya <mail2aditya.rana98@gmail.com>
2021-02-07 06:17:30 +0530

    Add abstract factory

---
+++ Design-Patterns/src/main/kotlin/abstract factory/AbstractFactory.kt
@@ -0,0 +1,73 @@
+package `abstract factory`
+
+interface Chat {
+    fun getChannels(): List<Channel>
+    fun getMessages(channelId: Long): List<Message>
+}
+
+data class Channel(
+    val id: Long,
+    val name: String,
+    val users: List<User>
+)
+
+data class Message(
+    val id: Long,
+    val text: String
+)
+
+sealed class User {
+    object Self : User()
+    class Other(val name: String) : User()
+}
+
+class SDKChatFactory : ChatFactory() {
+    override fun getChat() = TODO()
+}
+
+class FakeChatFactory : ChatFactory() {
+    override fun getChat() = FakeChat()
+}
+
+class FakeChat : Chat {
+    override fun getChannels(): List<Channel> {
+        return listOf(
+            Channel(
+                id = 1,
+                name = "First Channel",
+                users = listOf(User.Self, User.Other("Tom"))
+            )
+        )
+    }
+
+    override fun getMessages(channelId: Long): List<Message> {
+        return listOf(
+            Message(id = 1, text = "Hello"),
+            Message(id = 2, text = "How are you?")
+        )
+    }
+}
+
+class SDKChat : Chat {
+    override fun getChannels(): List<Channel> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getMessages(channelId: Long): List<Message> {
+        TODO("Not yet implemented")
+    }
+}
+
+abstract class ChatFactory {
+    abstract fun getChat(): Chat
+
+    companion object {
+        inline fun <reified T : Chat> create(): ChatFactory =
+            when (T::class) {
+                FakeChat::class -> FakeChatFactory()
+                SDKChat::class -> SDKChatFactory()
+                else -> throw IllegalArgumentException()
+            }
+    }
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Kotlin-Design-Patterns
+
+## Design patterns
+
+* [Creational Patterns](#creational)
+	* [Builder / Assembler](#builder--assembler)
+	* [Factory Method](#factory-method)
+	* [singleton](#singleton)
+	* [Abstract Factory](#abstract-factory)
+	
+* [Structural Patterns](#structural)
+	* [Adapter](#adapter)
+	* [Decorator](#decorator)
+	* [Facade](#facade)
+	* [Protection Proxy](#protection-proxy)
+	* [Composite](#composite)
+	
+* [Behavioral Patterns](#behavioral)
+	* [Observer / Listener](#observer--listener)
+	* [Strategy](#strategy)
+	* [Command](#command)
+	* [State](#state)
+	* [Chain of Responsibility](#chain-of-responsibility)
+	* [Visitor](#visitor)
+	* [Mediator](#mediator)
+	* [Memento](#memento)
