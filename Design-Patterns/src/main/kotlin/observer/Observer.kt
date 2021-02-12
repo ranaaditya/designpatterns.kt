2275e128dbdfd9464dccf3366b953df96fe8cdf7
ranaaditya <mail2aditya.rana98@gmail.com>
2021-02-12 16:28:28 +0530

    feat(observer-pattern): add observer pattern

---
+++ Design-Patterns/src/main/kotlin/observer/Observer.kt
@@ -0,0 +1,29 @@
+package observer
+
+import kotlin.properties.Delegates
+
+interface ChangeListener {
+    fun onChangeInitiated(oldValue: Any, changedValue: Any)
+}
+
+class ShowChangeListener: ChangeListener {
+    override fun onChangeInitiated(oldValue: Any, changedValue: Any) {
+        println("Value has been changed to ${changedValue.toString()} from ${oldValue.toString()}")
+    }
+}
+
+class ObservableObject(changeListener: ChangeListener) {
+    var text: String by Delegates.observable(
+            initialValue = "",
+            onChange = {
+                property, oldValue, newValue ->
+                changeListener.onChangeInitiated(oldValue, newValue)
+            }
+    )
+}
+
+fun main() {
+    val observableObject = ObservableObject(ShowChangeListener())
+    observableObject.text = "hello"
+    observableObject.text = "hallo"
+}
