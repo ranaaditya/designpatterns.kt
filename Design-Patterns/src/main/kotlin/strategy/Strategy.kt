abbb4f7d23910b50d3757762e93f9f6ed82f5757
ranaaditya <mail2aditya.rana98@gmail.com>
2021-02-12 17:46:14 +0530

    add strategy pattern

--- Design-Patterns/src/main/kotlin/strategy/Strategy.kt
+++ Design-Patterns/src/main/kotlin/strategy/Strategy.kt
@@ -1,2 +1,48 @@
 package strategy
 
+/**
+ * @author ranaaditya
+ *
+ * Strategy Design Pattern
+ */
+interface EncryptionStrategy {
+    fun encryptData(plainText: String?)
+}
+
+class AesEncryptionStrategy: EncryptionStrategy {
+    override fun encryptData(plainText: String?) {
+        println("Encrypting data using AES algorithm")
+    }
+}
+
+class RSAEncryptionStrategy: EncryptionStrategy {
+    override fun encryptData(plainText: String?) {
+        println("Encrypting data using RSA strategy")
+    }
+}
+
+class TripleDESStrategy: EncryptionStrategy {
+    override fun encryptData(plainText: String?) {
+        println("Encrypting data using TripleDES strategy")
+    }
+}
+
+class BlowFishEncryptionStrategy: EncryptionStrategy {
+    override fun encryptData(plainText: String?) {
+        println("Encrypting data using BlowFish strategy")
+    }
+}
+
+class Encryptor(strategy: EncryptionStrategy) {
+    private var strategy: EncryptionStrategy =strategy
+
+    fun encrypt(plainText: String?) {
+        strategy.encryptData(plainText)
+    }
+}
+
+fun main() {
+    val text = "hello world !"
+    val encryptor = Encryptor(TripleDESStrategy())
+    encryptor.encrypt(text)
+}
